5a9fea3b1744c5aa611555aa1d4b33329d1831bd
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-04-05 00:41:09 +0300

    Application class created and Hilt added

---
+++ app/src/main/java/com/hercan/rickandmortyapp/App.kt
@@ -0,0 +1,7 @@
+package com.hercan.rickandmortyapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/com/hercan/rickandmortyapp/ui/MainActivity.kt
+++ app/src/main/java/com/hercan/rickandmortyapp/ui/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.hercan.rickandmortyapp.databinding.ActivityMainBinding
 import com.hercan.rickandmortyapp.presentation.viewbinding.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val binding by viewBinding(ActivityMainBinding::inflate)
